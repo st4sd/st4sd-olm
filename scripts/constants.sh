d61a139d91a47cb247d1dbb39d3c760fb80a0b96
Vassilis Vassiliadis <43679502+VassilisVassiliadis@users.noreply.github.com>
2025-12-10 15:19:37 +0000

    build: bundle st4sd-deployment==2.5.5 for st4sd-olm==0.9.3 (#17)
    * build: bundle st4sd-deployment==2.5.5 for st4sd-olm==0.9.3
    
    
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
    
    ---------
    
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export VERSION="0.9.2"
+export VERSION="0.9.3"
 
 # VV: All versions that go in the "stable" channel, version[i+1] replaces version[i]
 STABLE_VERSIONS=(
@@ -8,4 +8,4 @@ STABLE_VERSIONS=(
     "0.0.8" "0.0.9" "0.0.10" "0.0.11" "0.0.12" "0.0.13"
     "0.0.14" "0.0.15" "0.0.16" "0.0.17" "0.0.18" "0.1.1" "0.1.2"
     "0.1.3" "0.2.0" "0.3.0" "0.4.0" "0.5.0" "0.6.0" "0.7.0"
-    "0.9.1 0.9.2")
+    "0.9.1 0.9.2 0.9.3")
